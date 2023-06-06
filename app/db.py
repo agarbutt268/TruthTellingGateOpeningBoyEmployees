6c5e17e967391d65d37eff065aeed0251d04f687
leriivt <verit.li@gmail.com>
2023-06-06 13:03:12 -0400

    basic login functions

--- app/db.py
+++ app/db.py
@@ -17,3 +17,59 @@ def refet_database():
 
     db.commit()
     db.close()
+
+
+#adds new user (username & password) into the database 
+def add_new_user(username, password, phone_number, email, bio):
+    
+    db = sqlite3.connect(DB_FILE) #open if file exists, if not it will create a new db
+    c = db.cursor() #creates db cursor to execute and fetch           
+
+    c.execute("SELECT COUNT(user_id) FROM users")
+
+    user_id = c.fetchone()
+
+    data = (user_id, username, password, phone_number, email, bio)
+
+
+    c.execute("INSERT INTO users VALUES(?,?,?,?,?,?)", data)
+    db.commit()
+    db.close()
+
+    #checks whether the user already exists in the database
+def check_user_exists(username):
+    db = sqlite3.connect(DB_FILE) #open if file exists, if not it will create a new db 
+    c = db.cursor() #creates db cursor to execute and fetch      
+
+    c.execute("SELECT * FROM users WHERE username=?", (username,))
+    dict = c.fetchone()
+
+    db.close()
+
+    if dict == None: #if no user
+        return False
+    
+    return True #if dict is not empty (meaning user exists)
+
+#gets the user's password from the database
+def get_user_password(username):
+    db = sqlite3.connect(DB_FILE) #open if file exists, if not it will create a new db          
+    c = db.cursor() #creates db cursor to execute and fetch      
+
+    c.execute("SELECT * FROM users WHERE username=?", (username,))
+    dict = c.fetchone()
+
+    db.close()
+
+    return dict[2]
+
+def get_user_id(username)
+    db = sqlite3.connect(DB_FILE) #open if file exists, if not it will create a new db          
+    c = db.cursor() #creates db cursor to execute and fetch      
+
+    c.execute("SELECT * FROM users WHERE username=?", (username,))
+    dict = c.fetchone()
+
+    db.close()
+
+    return dict[0]
